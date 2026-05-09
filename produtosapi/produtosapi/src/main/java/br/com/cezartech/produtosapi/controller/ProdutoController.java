9415c4a89e2cc83aafa9dbe214521729e8529918
evertoncg10 <evertoncg10@outlook.com>
2026-05-08 21:18:41 -0300

    Aula 10 - Criando endpoint para recebimento dos produtos.

---
+++ produtosapi/produtosapi/src/main/java/br/com/cezartech/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,18 @@
+package br.com.cezartech.produtosapi.controller;
+
+import br.com.cezartech.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto salvo: " + produto);
+        return produto;
+    }
+}
